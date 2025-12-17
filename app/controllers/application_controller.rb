ed58806e62d9641940c7ad3a0b6cc6f21ab1b803
Idiltatar <116079201+Idiltatar@users.noreply.github.com>
2025-12-17 14:08:52 +0000

    Fixing errors after ci/cd error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-  
   allow_browser versions: :modern
   before_action :authenticate_user!
   helper :ui
--- app/helpers/ui_helper.rb
+++ app/helpers/ui_helper.rb
@@ -5,18 +5,17 @@ module UiHelper
         "In Progress" => "info",
         "Closed" => "success"
       }[status.to_s] || "secondary"
-  
+
       content_tag(:span, status, class: "badge text-bg-#{color}")
     end
-  
+
     def priority_badge(priority)
       color = {
         "Low" => "success",
         "Medium" => "primary",
         "High" => "danger"
       }[priority.to_s] || "secondary"
-  
+
       content_tag(:span, priority, class: "badge text-bg-#{color}")
     end
-  end
-  
+end
